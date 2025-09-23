8853e6156a4366551dc8aac60d00b72214419903
sampath <sampathkocherla7@gmail.com>
2025-09-23 21:07:25 +0530

    User model fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 
 import { Inngest } from "inngest";
-import User from "../models/User";
+import User from "../models/User.js";
 //create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
